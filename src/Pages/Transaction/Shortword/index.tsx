4615e7ce9049f1830f716bd9a1721f07c5b0b610
liuboheng <lbh9311@sina.com>
2020-01-10 11:23:09 +0800

    fix Shortword UI

--- src/Pages/Transaction/Shortword/index.tsx
+++ src/Pages/Transaction/Shortword/index.tsx
@@ -9,217 +9,208 @@ import {
   TouchableOpacity,
   Slider,
   StatusBar,
+  EmitterSubscription,
+  Keyboard,
 } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {
   NavigationStackScreenProps,
   NavigationStackOptions,
 } from 'react-navigation-stack';
-import {observer} from 'mobx-react';
+import {observer, inject} from 'mobx-react';
 import {observable, action} from 'mobx';
 import {withTranslation, WithTranslation} from 'react-i18next';
 import {I18nTransactionType} from 'I18n/config';
-
-const styles = StyleSheet.create({
-  mainWrapper: {
-    // backgroundColor: '#fafbfc',
-    backgroundColor: '#aaa',
-    flex: 1,
-  },
-  wrapper: {
-    flex: Platform.OS === 'android' ? 0 : 1,
-  },
-  toAddressWrapper: {
-    backgroundColor: '#fff',
-    marginTop: 20,
-  },
-  toAddressLabel: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    fontSize: 30,
-  },
-  toAddressInput: {
-    alignSelf: 'center',
-    width: '95%',
-    overflow: 'hidden',
-    fontSize: 18,
-  },
-  sendAmountWrapper: {
-    backgroundColor: '#fff',
-    marginTop: 20,
-  },
-  sendAmountBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  sendAmountInput: {
-    alignSelf: 'center',
-    width: '95%',
-    overflow: 'hidden',
-    fontSize: 18,
-  },
-  ExtraDataWrapper: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    paddingLeft: 15,
-  },
-  txFeeWrapper: {
-    backgroundColor: '#fff',
-    marginTop: 20,
-  },
-  txFeeBar: {
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  txFeeBottomBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  btnWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 30,
-    height: 50,
-    borderRadius: 25,
-  },
-  btnSend: {
-    height: 30,
-    // borderRadius: 15,
-  },
-});
+import TransactionStore from 'Store/transaction';
+import {styles} from './config';
+import Toast from 'Components/Toast';
+import Modal from 'Components/Modal';
+import {sleep} from 'Global/utils';
+import WalletStore from 'Store/wallet';
 
 interface Props {
   navigation: NavigationStackScreenProps['navigation'];
   labels: I18nTransactionType;
+  transaction?: TransactionStore;
+  wallet?: WalletStore;
 }
 
+@inject('transaction', 'wallet')
 @observer
 class Shortword extends React.Component<Props> {
-  validateShortword(word: string): boolean {
-    return true;
-  }
-
-  validateEnteringAmount(amountString: string) {
-    const reg = new RegExp('^[0-9]*(.[0-9]{0,18})?$');
-    return reg.test(amountString);
-  }
-
-  validateExtraData(text: string) {
-    if (text.length > 200) {
-      return false;
-    }
-    // --- add new rule here
-    return true;
-  }
-
   @observable shortword: string = '';
-  @observable receiveAmount: string = '';
-  @observable extraData: string = '';
+  @observable txFeeLevel: number = 1;
+  @observable keyboardShow: boolean = false;
+  keyboardDidShowListener: EmitterSubscription;
+  keyboardDidHideListener: EmitterSubscription;
+  constructor(props: Props) {
+    super(props);
+    this.keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      this.keyboardDidShow,
+    );
+    this.keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      this.keyboardDidHide,
+    );
+  }
+
+  @action
+  keyboardDidShow = () => {
+    this.keyboardShow = true;
+  };
+
+  @action
+  keyboardDidHide = () => {
+    this.keyboardShow = false;
+  };
 
   @action handleChangeShortword = (text: string) => {
-    if (this.validateShortword(text)) {
-      this.shortword = text;
-    }
+    this.shortword = text;
   };
-  @action handleChangeSendAmount = (amountString: string) => {
-    if (this.validateEnteringAmount(amountString)) {
-      this.receiveAmount = amountString;
-    }
+
+  @action handleChangeTxfee = (num: number) => {
+    this.txFeeLevel = num;
   };
-  @action handleChangeExtraData = (text: string) => {
-    if (this.validateExtraData(text)) {
-      this.extraData = text;
+
+  sendTransaction = async () => {
+    try {
+      const res = await this.props.transaction!.confirmTransaction(
+        this.shortword,
+        '0',
+        '',
+        '21000',
+        '1',
+      );
+      if (res.success) {
+        console.warn('success');
+        return Promise.resolve();
+      } else {
+        console.warn(res.info);
+        return Promise.reject();
+      }
+    } catch (e) {
+      return Promise.reject();
     }
   };
 
   handleSend = () => {
-    console.warn(this.shortword);
+    Modal.password(this.handleConfirmTransaction);
+    // this.setPasswordModal(true);
+    // this.sendTransaction();
   };
+
+  handleConfirmTransaction = async (psw: string) => {
+    Modal.hide();
+    Toast.loading();
+    await sleep(1000);
+    Toast.hide();
+    if (!this.props.wallet!.checkPassword(psw)) {
+      Toast.hide();
+      Toast.info(this.props.labels.passwordError);
+      return;
+    }
+
+    try {
+      await this.sendTransaction();
+      Toast.hide();
+      Toast.success(this.props.labels.sendSuccess);
+      return;
+    } catch (e) {
+      Toast.hide();
+      Toast.info(this.props.labels.sendFailure);
+    }
+  };
+
   render() {
-    const {labels} = this.props;
     return (
       <View style={styles.mainWrapper}>
         <StatusBar backgroundColor="#fff" />
+
         <KeyboardAwareScrollView
           contentContainerStyle={styles.wrapper}
-          style={styles.mainWrapper}
-          testID={'first-incoming-transaction-screen'}
+          style={styles.contentWrapper}
           resetScrollToCoords={{x: 0, y: 0}}>
-          <TouchableOpacity style={styles.toAddressWrapper} activeOpacity={0.8}>
-            <View style={styles.toAddressLabel}>
-              <Text>{labels.Shortword}</Text>
-            </View>
-            <TextInput
-              style={styles.toAddressInput}
-              value={this.shortword}
-              onChangeText={this.handleChangeShortword}
-              placeholder={labels.enterRegisterShortword}
-            />
-          </TouchableOpacity>
+          {this.renderShortwordBox()}
 
-          <TouchableOpacity
-            style={styles.sendAmountWrapper}
-            activeOpacity={0.8}>
-            <TextInput
-              style={styles.sendAmountInput}
-              value={this.receiveAmount}
-              onChangeText={this.handleChangeSendAmount}
-              placeholder={labels.enterReceiveAmount}
-              keyboardType="numeric"
-            />
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.ExtraDataWrapper} activeOpacity={0.8}>
-            <Text style={{height: '100%', lineHeight: 50, fontSize: 18}}>
-              {labels.remarkOptional}
-            </Text>
-            <TextInput
-              style={styles.sendAmountInput}
-              value={this.extraData}
-              onChangeText={this.handleChangeExtraData}
-              placeholder={labels.ehterRemark}
-            />
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.txFeeWrapper} activeOpacity={0.8}>
-            <View style={styles.txFeeBar}>
-              <Text>{labels.txFee}</Text>
-              <Text>1.060494606 DIP ≈ $ 0.63</Text>
-            </View>
-            <Slider minimumValue={1} maximumValue={3} step={1} />
-            <View style={styles.txFeeBottomBar}>
-              <Text>{labels.low}}</Text>
-              <Text>{labels.middle}</Text>
-              <Text>{labels.high}</Text>
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.btnWrapper}
-            onPress={this.handleSend}
-            activeOpacity={0.8}>
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: 400,
-                height: 40,
-                borderRadius: 20,
-                backgroundColor: '#149bd5',
-              }}>
-              <Text style={{color: '#fff', fontSize: 17}}>
-                {labels.sendShortword}
-              </Text>
-            </View>
-          </TouchableOpacity>
+          {this.renderTxFeeBox()}
         </KeyboardAwareScrollView>
+        {!this.keyboardShow && this.renderBtnBox()}
       </View>
+    );
+  }
+
+  renderShortwordBox() {
+    const {labels} = this.props;
+    return (
+      <TouchableOpacity style={styles.toAddressWrapper} activeOpacity={0.8}>
+        <View style={styles.toAddressLabel}>
+          <Text style={styles.toAddressText}>{labels.shortword}</Text>
+        </View>
+        <TextInput
+          style={styles.toAddressInput}
+          value={this.shortword}
+          onChangeText={this.handleChangeShortword}
+          placeholder={labels.enterRegisterShortword}
+        />
+      </TouchableOpacity>
+    );
+  }
+
+  renderTxFeeBox() {
+    const {labels} = this.props;
+    return (
+      <TouchableOpacity style={styles.txFeeWrapper} activeOpacity={0.8}>
+        <View style={styles.txFeeBar}>
+          <Text style={styles.txFeeLabel}>{labels.txFee}</Text>
+          <Text style={styles.txFeeText}>0.000000000000021 DIP</Text>
+        </View>
+        <Slider
+          minimumValue={1}
+          maximumValue={3}
+          step={1}
+          onValueChange={this.handleChangeTxfee}
+        />
+        <View style={styles.txFeeBottomBar}>
+          <Text
+            style={
+              this.txFeeLevel >= 1
+                ? styles.activeFeeLevel
+                : styles.defalutFeeLevel
+            }>
+            {labels.low}
+          </Text>
+          <Text
+            style={
+              this.txFeeLevel >= 2
+                ? styles.activeFeeLevel
+                : styles.defalutFeeLevel
+            }>
+            {labels.middle}
+          </Text>
+          <Text
+            style={
+              this.txFeeLevel >= 3
+                ? styles.activeFeeLevel
+                : styles.defalutFeeLevel
+            }>
+            {labels.high}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+  renderBtnBox() {
+    const {labels} = this.props;
+    return (
+      <TouchableOpacity
+        style={styles.btnWrapper}
+        onPress={this.handleSend}
+        activeOpacity={0.8}>
+        <View style={styles.btnView}>
+          <Text style={styles.btnText}>{labels.sendShortword}</Text>
+        </View>
+      </TouchableOpacity>
     );
   }
 }
