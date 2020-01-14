4fa1065246b96c9c81f335ac75e00addc0d7b5f2
yangkangwei <yangkangwei@mintechai.com>
2020-01-14 16:32:01 +0800

    fanyi

--- src/Pages/Accounts/TransactionDetail/index.tsx
+++ src/Pages/Accounts/TransactionDetail/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { View, Text, StyleSheet } from "react-native"
 import { NavigationScreenProp } from "react-navigation"
+import moment from "moment"
 
 import { I18nAccountType } from 'I18n/config'
 import { WithTranslation, withTranslation } from 'react-i18next'
@@ -26,7 +27,7 @@ class TransactionDetail extends React.Component<Props>{
                 </View>
                 <View style={styles.item}>
                     <Text style={styles.itemLabel}>{labels.timeStamp}:</Text>
-                    <Text style={styles.itemValue}>{timestamp}</Text>
+                    <Text style={styles.itemValue}>{moment(timestamp/1000000).format('YYYY/MM/DD HH:MM:SS')}</Text>
                 </View>
                 <View style={styles.item}>
                     <Text style={styles.itemLabel}>{labels.nonce}:</Text>
